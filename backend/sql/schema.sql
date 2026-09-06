b7e03fc2d4c84dd5f179fb5e67a0ef978ea76ce9
Raphael Americano <raphael@raphaelamericano.com.br>
2026-09-06 16:12:36 -0300

    Update card rarity options in schema.sql to include additional rarity types
    - Expanded the rarity ENUM in the card table to include new categories such as 'mythic_rare', 'reverse_holo', 'ultra_rare', and others for better card classification.

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -11,7 +11,13 @@ CREATE TABLE IF NOT EXISTS card (
     card_set CHAR(36) NOT NULL, 
     card_game ENUM('magic', 'yugioh', 'pokemon') NOT NULL,
     img_url VARCHAR(500),
-    rarity ENUM('common', 'uncommon', 'rare', 'mythic', 'legendary') NOT NULL
+    rarity ENUM(
+    'common', 'uncommon', 'rare', 'mythic_rare',
+    'reverse_holo', 'rare_holo', 'ultra_rare', 'illustration_rare',
+    'special_illustration_rare', 'hyper_rare', 'promotional',
+    'super_rare', 'secret_rare', 'ultimate_rare', 'collectors_rare',
+    'ghost_rare', 'starlight_rare', 'quarter_century_secret_rare'
+) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS collection (
