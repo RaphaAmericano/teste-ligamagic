b4837f9cba660210e14007314e8862eb301e6f5c
Raphael Americano <raphael@raphaelamericano.com.br>
2026-09-07 02:40:32 -0300

    Add image validation to new card form submission

--- frontend/js/cards.js
+++ frontend/js/cards.js
@@ -13,12 +13,13 @@ function validateNewCardForm(data){
     const card_set = data.get('card_set');
     const rarity = data.get('rarity');
     const image = data.get('image');
-
+    console.log(image)
     if(!name_pt) errors.push('Nome em português é obrigatório')
     if(!name_en) errors.push('Nome em inglês é obrigatório')
     if(!CARD_GAMES.has(card_game)) errors.push('Card game inválido')
     if(!card_set) errors.push('Edição é obrigatória')
     if(!rarity) errors.push('Raridade é obrigatória')
+    if(!image || image.size === 0) errors.push('Imagem da carta é obrigatória')
 
     if(image && image.size > 0 ){
         if(!ALLOWED_IMAGE_TYPES.includes(image.type)){
