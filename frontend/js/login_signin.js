7223b386f3b6b1cef3832b9f6b90d4b944ff9c38
Raphael Americano <raphael@raphaelamericano.com.br>
2026-09-08 17:25:42 -0300

    Enhance authentication process and user feedback
    - Updated Auth class to return a JWT token upon successful user creation.
    - Modified AuthController to ensure proper error handling and response formatting.
    - Improved frontend login/signin functionality with enhanced error messaging and form state management.

--- frontend/js/login_signin.js
+++ frontend/js/login_signin.js
@@ -2,8 +2,10 @@ import { submitLogin, submitSignin, checkJwtToken } from "./auth.js"
 
 function toggleFormLoader(){
     const form = document.querySelector('.main-form')
+    const submitButton = document.getElementById('signinSubmitButton');
     const loader = document.querySelector('.loader')
-    form.classList.toggle('hidden')
+    submitButton.disabled = !submitButton.disabled
+    form.disabled = !form.disabled
     loader.classList.toggle('hidden')
 }
 
@@ -12,22 +14,37 @@ const actions = {
     signin: submitSignin
 }
 
+function setWaringMessages(message){
+    const messageBox = document.querySelector('.loader')
+    console.log(messageBox)
+    const [h4Tag] = messageBox.children;
+    console.log(h4Tag)
+    h4Tag.innerText = message;
+    console.log(message)
+}
+
 async function submitForm(event){
     event.preventDefault()
     toggleFormLoader()
     const action = event.submitter.dataset.action
     const fn = actions[action]
+    let hasError = false;
+
     try {
         const response = await fn(event.target.elements)
+        console.log(response)
         if(response.token){
             localStorage.setItem('ligamagicJwtToken', response.token)
             window.location.href = "./dashboard.html"
         }
     } catch (error) {
         console.warn(error)
-        alert(error.message)
+        hasError = true;
+
+        setWaringMessages(error || 'Error ao realizar a operação');
     } finally {
-        toggleFormLoader()
+        const delay = hasError ? 3000 : 0;
+        setTimeout(() => toggleFormLoader(), delay)
     }
 }
 
